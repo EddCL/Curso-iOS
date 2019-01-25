85b167580875581f1e038a69c6757929e068f78e
EddCL <38968021+EddCL@users.noreply.github.com>
2019-01-25 14:47:34 -0600

    Add files via upload

---
+++ 01-IAmRich/01-IAmRich/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01-IAmRich
+//
+//  Created by Eduardo Carrillo on 1/25/19.
+//  Copyright © 2019 Eduardo Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
