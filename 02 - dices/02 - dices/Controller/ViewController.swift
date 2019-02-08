17c336ce0a45c2dc8c29efffad7da684dc3f5fe3
Eduardo Carrillo <EduardoS.S@iMac-de-iMac-3.local>
2019-02-08 14:21:50 -0600

    implementación de la función aleatoria para el botón

--- 02 - dices/02 - dices/Controller/ViewController.swift
+++ 02 - dices/02 - dices/Controller/ViewController.swift
@@ -13,15 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceLeft: UIImageView!
     @IBOutlet weak var diceRight: UIImageView!
     
+    var randomDiceIndexLeft: Int = 0
+    var randomDiceINdexRight: Int = 0
+    let diceImages: [String] = ["uno", "dos", "tres", "cuatro", "cinco", "seis"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
+        generateRandomDices()
     }
 
     @IBAction func RollPressed(_ sender: UIButton) {
+        generateRandomDices()
+    }
+    
+    func generateRandomDices(){
+        let nFaces: UInt32 = UInt32(diceImages.count) //para asegurar que no crashee la app por si llegaramos a cambiar el numero de elementos de nuestro arreglo diceImages
         
+        randomDiceIndexLeft = Int(arc4random_uniform(nFaces)) //casteo de uInt32 a Int;
+        randomDiceINdexRight = Int(arc4random_uniform(nFaces)) //arc4random_uniform devuelve un entero de 32 bits sin signo
+        
+        diceLeft.image = UIImage(named: diceImages[randomDiceIndexLeft])
+        diceRight.image = UIImage(named: diceImages[randomDiceINdexRight])
     }
     
 }
