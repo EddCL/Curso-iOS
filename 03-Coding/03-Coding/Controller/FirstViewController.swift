d3ffebe14559bc9070864c8df8b39c113fcc1a54
Eduardo Carrillo <EduardoS.S@iMac-de-iMac-3.local>
2019-02-11 19:31:05 -0600

    Colocación del slider y el label para indicar la edad del usuario

--- 03-Coding/03-Coding/Controller/FirstViewController.swift
+++ 03-Coding/03-Coding/Controller/FirstViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class FirstViewController: UIViewController, UITextFieldDelegate {
-
+    
+    @IBOutlet weak var labelAge: UILabel!
+    
+    var userAge = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    
+    //para implementar, recordar arrastrar el text field al view controler y darle en DELEGATE
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() //para quitar el teclado al dar a INTRO
+        print("boton intro presionado")
+        
+        //Recuperar el contenido del text field
+        if let text = textField.text{
+            print(text)
+        }
+        return true
+    }
 
-
+    @IBAction func sliderAgeMoved(_ sender: UISlider) {
+        print(Int(sender.value))
+        userAge = Int(sender.value)
+        
+        self.labelAge.text = "\(userAge)"
+    }
+    
 }
 
