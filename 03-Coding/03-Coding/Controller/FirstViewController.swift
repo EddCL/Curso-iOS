fcef7f178cf71c1824ebfc6f16dc2f1e13bc6a45
Eduardo Carrillo <EduardoS.S@iMac-de-iMac-3.local>
2019-02-13 13:55:18 -0600

    Continuación proyecto 3

--- 03-Coding/03-Coding/Controller/FirstViewController.swift
+++ 03-Coding/03-Coding/Controller/FirstViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class FirstViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var labelAge: UILabel!
+    @IBOutlet weak var sliderAge: UISlider!
     
     var userAge = -1
+    var userName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateAgeLabel()
     }
     
     
@@ -26,16 +28,33 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         print("boton intro presionado")
         
         //Recuperar el contenido del text field
-        if let text = textField.text{
-            print(text)
+        if let name = textField.text{
+            print(name)
+            self.userName = name
         }
         return true
     }
 
     @IBAction func sliderAgeMoved(_ sender: UISlider) {
         print(Int(sender.value))
-        userAge = Int(sender.value)
-        
+        self.updateAgeLabel()
+    }
+    
+    @IBAction func validateData(_ sender: UIButton) {
+        if userName == "Juan Gabriel"{
+            print("Puedes entrar por ser JB :v")
+            self.view.backgroundColor = .green
+        }else if userAge >= 18 {
+            print("Puedes entrar por ser mayor :B")
+            self.view.backgroundColor = .green
+        }else{
+            print("No puedes entrar :(")
+            self.view.backgroundColor = .red
+        }
+    }
+    
+    func updateAgeLabel() {
+        userAge = Int(sliderAge.value)
         self.labelAge.text = "\(userAge)"
     }
     
