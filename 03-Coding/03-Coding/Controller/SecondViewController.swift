f068b807b4aa56658cae828be72fdbf6e9c6b9ee
Eduardo Carrillo <EduardoS.S@imac-de-imac-3.local>
2019-02-15 13:40:48 -0600

    Funcion para numero de fibonnacci

--- 03-Coding/03-Coding/Controller/SecondViewController.swift
+++ 03-Coding/03-Coding/Controller/SecondViewController.swift
@@ -12,7 +12,7 @@ class SecondViewController: UIViewController {
     
     var fibonacci : [Int] = [0,1]
     
-    var fibonacciID = 1
+    var fibonacciID = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,14 @@ class SecondViewController: UIViewController {
         if fibonacciID <= 1 || fibonacciID > 100{
             return
         }
+        
+        //Generar numeros de fibonacci hasta fibonacciID
+        
+        for i in 2...fibonacciID {
+            fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+        }
+        
+        print(fibonacci)
     }
 
 
