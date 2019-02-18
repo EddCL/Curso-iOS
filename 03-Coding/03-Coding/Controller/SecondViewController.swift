aba9e1d112e7f40a46f2ec66965e8ce2dd5966cc
Eduardo Carrillo <EduardoS.S@imac-de-imac-3.local>
2019-02-18 16:56:19 -0600

    Segunda vista de la app terminada

--- 03-Coding/03-Coding/Controller/SecondViewController.swift
+++ 03-Coding/03-Coding/Controller/SecondViewController.swift
@@ -10,27 +10,43 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var labelNumber: UILabel!
+    @IBOutlet weak var textViewResult: UITextView!
+    @IBOutlet weak var stepper: UIStepper!
+    
     var fibonacci : [Int] = [0,1]
     
     var fibonacciID = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateLabel(id: Int(self.stepper.value))
     }
     
     func generateFibonacciNumbers(){
-        if fibonacciID <= 1 || fibonacciID > 100{
-            return
-        }
-        
         //Generar numeros de fibonacci hasta fibonacciID
-        
+        fibonacci = [0,1]
         for i in 2...fibonacciID {
             fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
         }
+        let fibStr : [String] = fibonacci.compactMap({String($0)})
+        let result : String = fibStr.joined(separator: "\n")
         
-        print(fibonacci)
+        textViewResult.text = "\(result)"
     }
+    
+    func updateLabel(id: Int){
+        self.fibonacciID = id
+        self.labelNumber.text = "\(self.fibonacciID)"
+        generateFibonacciNumbers()
+    }
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+       updateLabel(id: Int(sender.value))
+    }
+    
+    
 
 
 }
