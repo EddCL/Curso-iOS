1ed5281379842840b9ca5869435a4b87bf2d1258
Eduardo Carrillo <EduardoS.S@iMac-de-iMac-3.local>
2019-03-21 17:34:23 -0600

    Casi finalizado, proyecto 4

--- 04 - Piano/04 - Piano/ViewController.swift
+++ 04 - Piano/04 - Piano/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    var audioPlayer: AVAudioPlayer!
     let soundsArray = ["c1", "c1s", "d1", "d1s", "e1", "f1", "f1s", "g1", "g1s", "a1", "a1s", "b1", "c2"]
 
     override func viewDidLoad() {
@@ -19,8 +21,18 @@ class ViewController: UIViewController {
 
     @IBAction func playSound(_ sender: UIButton) {
         let idKey = sender.tag
-        let fileName = soundsArray[idKey - 1]
-    print("tecla \(idKey)/\(fileName) pulsada")
+        let fileName = soundsArray[idKey-1]
+        print("tecla \(idKey)/\(fileName) pulsada")
+        
+        if let soundURL : URL = Bundle.main.url(forResource: fileName, withExtension: "mp3"){
+            print(soundURL)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            } catch{
+                print(error)
+            }
+            audioPlayer.play()
+        }
     }
     
 }
