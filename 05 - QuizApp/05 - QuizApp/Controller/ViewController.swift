f0e7cbc9ad4c67c0e9a43218024536b001d7516f
Eduardo Carrillo López <ed.carrillo.l97@hotmail.com>
2019-03-27 12:29:39 -0600

    Continuación con proyecto 5

--- 05 - QuizApp/05 - QuizApp/Controller/ViewController.swift
+++ 05 - QuizApp/05 - QuizApp/Controller/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var progressBar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("\(sender.tag)")
+    }
+    
 }
 
